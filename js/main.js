48eb0234e63501f045f211d6ac6ffd634f5f0f4c
Believerockss <64014691+Believerockss@users.noreply.github.com>
2022-07-12 14:12:43 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//Change Navbar on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 100)
+})
+
+//Show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //Change Icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus'
+        } else {
+            icon.className = 'uil uil-plus'
+        }
+    })
+})
+
+
+//Show/Hide nav menu 
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+//Close Nav Menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
